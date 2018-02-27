a3b24389832a4d3159f93c68ff3e457e6739c7a7
Kris Sakarias <ksakarias@gmail.com>
2018-02-27 12:53:47 -0800

    working on fetch-all

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,8 +2,8 @@
 
 var app = {};
 
-var __API_URL__ = 'http://localhost:3000';
-// var __API_URL__ = 'https://git.heroku.com/co-ks-booklist.git';
+// var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://git.heroku.com/co-ks-booklist.git';
 
 (module => {
   Book.all = [];
@@ -14,17 +14,25 @@ var __API_URL__ = 'http://localhost:3000';
   }
 
   let book = new Book({
-    'title': 'test',
-    'author': 'kris',
-    'isbn': '1111',
-    'image_url': 'www.test.com',
-    'description': 'test'
+    title: 'test',
+    author: 'kris',
+    isbn: '1111',
+    image_url: 'www.test.com',
+    description: 'test'
   });
 
   Book.prototype.toHtml = function() {
     let template = Handlebars.compile($('#book-list-template').text());
     return template(this);
   };
+
+  // Book.loadAll = (rows) =>
+
+
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then(result => console.log(result));
+
 
   console.log(book);
   module.Book = Book;
