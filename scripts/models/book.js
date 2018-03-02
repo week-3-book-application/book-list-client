b12d0b7a1777d53c45284e1e10113f94f25df21e
Carl Olson <colson10@gmail.com>
2018-03-02 09:38:33 -0800

    deploy

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,7 +3,7 @@
 var app = app || {};
 
 // const __API_URL__ = 'http://localhost:3000';
-const __API_URL__ = 'https://git.heroku.com/co-ks-booklist.git';
+const __API_URL__ = 'https://co-ks-booklist.herokuapp.com';
 
 (module => {
   function throwErr(err) {
